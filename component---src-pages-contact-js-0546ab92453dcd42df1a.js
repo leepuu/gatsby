6212e76d8127a85ea9090fca9092b0b5d43ab9cf
leepuu <leepuu001@naver.com>
2023-02-06 09:32:47 +0900

    Updates

---
+++ component---src-pages-contact-js-0546ab92453dcd42df1a.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[10],{"Cuy+":function(e,n,a){"use strict";a.r(n);var t=a("q1tI"),l=a.n(t);n.default=function(){return l.a.createElement("div",null,l.a.createElement("h1",null,"Talk to me"),l.a.createElement("p",null,l.a.createElement("a",{href:"123123123"},"sdadasd")))}}}]);
+//# sourceMappingURL=component---src-pages-contact-js-0546ab92453dcd42df1a.js.map
